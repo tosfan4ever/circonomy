9b23a1ef936fac9da6c56717c7bfa1f39a67ddf3
cjustinobi <cjustinobi@gmail.com>
2021-11-26 14:59:51 +0100

    Responsive magic link page

--- src/components/MagicLink/MagicLink.styles.js
+++ src/components/MagicLink/MagicLink.styles.js
@@ -25,7 +25,10 @@ export const Content = styled.div`
   grid-template-columns: 60% 40%;
   
   @media (max-width: 767px) {
-   
+    width: 80%;
+    position: relative;
+    margin-top: 200px;
+    grid-template-columns: 100%;
   }
 `
 
@@ -33,15 +36,16 @@ export const WelcomeBack = styled.div`
   background: var(--brand-color);
   
   @media (max-width: 767px) {
-   
+   padding-bottom: 30px;
   }
 `
 
 export const MagicLinkWrapper = styled.div`
   background: #fefefe;
   padding: 0 50px 50px 50px;
+  
   @media (max-width: 767px) {
-   
+    padding: 0 10px 50px 10px;
   }
 `
 
@@ -57,8 +61,10 @@ export const Heading = styled.h1`
 export const Text = styled.p`
   margin: 40px 0;
   font-size: 1.4rem;
+  
   @media (max-width: 767px) {
-   
+   padding: 0 10px;
+   margin-bottom: 10px;
   }
 `
 
